b671b76ae94ce4689d4fbcbdbae7aa4efdcaa6eb
Leonardo Belissimo Muto <leonardo.muto@usc.br>
2022-09-15 10:44:08 -0300

    sucess page return to form

--- landinator/settings.py
+++ landinator/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
-ALLOWED_HOSTS = ['localhost', '0.0.0.0']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 # Application definition
 
